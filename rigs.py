78d2103df9af7bb0c0ccf231d30cc8c3e0c820bd
NiallEgan <negan1998@gmail.com>
2017-08-08 18:47:42 +0100

    Initial commit

---
+++ rigs.py
@@ -0,0 +1,21 @@
+""" Some useful semirings """
+
+class IntRig(object):
+    one = 1
+    zero = 0
+
+    def mult(self, x, y):
+        return x * y
+
+    def plus(self, x, y):
+        return x + y
+
+class BoolRig(object):
+    one = True
+    zero = False
+
+    def mult(self, x, y):
+        return x and y
+
+    def plus(self, x, y):
+        return x or y
